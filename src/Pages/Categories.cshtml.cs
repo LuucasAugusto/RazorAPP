33d579f460527e83c9498d7f2236f15261d43141
Lucas Augusto <lucas.fernandes0522@gmail.com>
2023-10-12 12:56:31 -0300

    Criando paginação, parâmetros e rotas

--- src/Pages/Categories.cshtml.cs
+++ src/Pages/Categories.cshtml.cs
@@ -7,14 +7,20 @@ namespace RazorAPP.Pages
     {
         public List<Category> Categories { get; set; } = new();
 
-        public void OnGet()
+        //Apenas para teste rapido
+        List<Category> temp = new List<Category>();
+
+        public void OnGet(int skip = 0, int take = 25)
         {
-            for (var i = 0; i <= 100; i++)
+            for (var i = 0; i <= 1787; i++)
             {
-                Categories.Add(new Category(i,
+                temp.Add(new Category(i,
                     $"Categoria {i}",
                     i * 18.95m));
             }
+
+            //Apenas para teste rapido
+            Categories = temp.Skip(skip).Take(take).ToList();
         }
     }
 
